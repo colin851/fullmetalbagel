06d7cf1b4915ef353608c80d3ef754fd6b1e2ecd
colin851 <346508928@qq.com>
2024-05-12 14:40:13 +0800

    update

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,27 @@
 
-const menu = document.querySelector(".menu-block");
-menu.addEventListener("click", () => {
-    const menuMask = document.querySelector(".menu-mask");
-    const result = menuMask.classList.toggle('showMenu');
-    if (result) {
-        document.body.style.cssText+= 'overflow: hidden';
-    } else {
-        document.body.style.removeProperty('overflow');
-    }
-});
+
+(function() {
+
+    const menu = document.querySelector(".menu-block");
+    menu.addEventListener("click", () => {
+        const menuMask = document.querySelector(".menu-mask");
+        const result = menuMask.classList.toggle('showMenu');
+        if (result) {
+            document.body.style.cssText+= 'overflow: hidden';
+        } else {
+            document.body.style.removeProperty('overflow');
+        }
+    });
+
+    const navBlock = document.querySelector(".menu-mask");
+    navBlock.addEventListener("click", (e) => {
+        if (e.target.tagName.toLowerCase() === "a") {
+            const curItem = navBlock.querySelector('.cur');
+            curItem.classList.remove('cur');
+            e.target.classList.add('cur');
+            const menuMask = document.querySelector(".menu-mask");
+            menuMask.classList.remove('showMenu');
+            document.body.style.removeProperty('overflow');
+        }
+    });
+})()
